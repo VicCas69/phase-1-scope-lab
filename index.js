1b260a3e92756b3d4e703b6813f6bb929708bd17
victor castro <victor_castro_01@hotmail.com>
2021-08-17 21:20:59 -0400

    assigment completed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = "bob";// Write your solution in this file!
+const leastFavotiteCustomer = "Elsa";
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(n="not bob"){
+    bestCustomer = n;
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+    return setBestCustomer("maybe bob")    
+}
+
+function changeLeastFavoriteCustomer(){
+    return leastFavotiteCustomer = "victor"
+}
